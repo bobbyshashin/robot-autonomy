a72f5ddb281cecc2e53535b12764f09ba0999398
Changsheng Shen <changshs@andrew.cmu.edu>
2019-02-13 16:45:16 -0500

    Jacobian calculation finished

--- hw1_release/code/forward_kinematics.py
+++ hw1_release/code/forward_kinematics.py
@@ -17,7 +17,7 @@ def constructTransformationMatrix(R, t):
 
     return T
 
-def getWristPose(joint_angle_list, link_translation, link_rotation):
+def getWristPose(joint_angle_list, rotational_axes, link_translation):
     '''Get the wrist pose for given joint angle configuration.
 
     joint_angle_list: List of joint angles to get final wrist pose for
@@ -31,21 +31,9 @@ def getWristPose(joint_angle_list, link_translation, link_rotation):
     obtained from the End-Effector Transformation matrix using column-major
     ordering.
     '''
-    # TODO("Complete this")
-    
-    # Read from URDF file
-    # link_translation = np.array([[0,      0,      0.072],
-    #                         [0,      0,      0.04125],
-    #                         [0.05,   0,      0.2],
-    #                         [0.2002, 0,      0],
-    #                         [0.063,  0.0001, 0]])
-
-    # link_rotation = np.array([[0, 0, 1*joint_angle_list[0]],
-    #                      [0, 1*joint_angle_list[1],0],
-    #                      [0, 1*joint_angle_list[2],0],
-    #                      [0, 1*joint_angle_list[3],0],
-    #                      [-1*joint_angle_list[4],0,0]])
     num_joints = len(joint_angle_list)
+    link_rotation = np.multiply(rotational_axes, np.tile(np.array(joint_angle_list).reshape(num_joints, 1), (1, 3)))
+
     T_total = np.eye(4)
 
     for i in range(num_joints):
@@ -54,7 +42,7 @@ def getWristPose(joint_angle_list, link_translation, link_rotation):
     # flatten to 1*16 column major
     return T_total.flatten('F')
 
-def getWristJacobian(joint_angle_list, link_translation, link_rotation):
+def getWristJacobian(joint_angle_list, rotational_axes, link_translation):
     '''Get the wrist jacobian for given joint angle configuration.
 
     joint_angle_list: List of joint angles to get final wrist pose for
@@ -69,61 +57,54 @@ def getWristJacobian(joint_angle_list, link_translation, link_rotation):
     obtained from the End-Effector Transformation matrix using column-major
     ordering.
     '''
-    # TODO("Complete this")
-    # Read from URDF file
-    # link_translation = np.array([[0,      0,      0.072],
-    #                         [0,      0,      0.04125],
-    #                         [0.05,   0,      0.2],
-    #                         [0.2002, 0,      0],
-    #                         [0.063,  0.0001, 0]])
+    num_joints = len(joint_angle_list)
 
-    # link_rotation = np.array([[0, 0, 1*joint_angle_list[0]],
-    #                      [0, 1*joint_angle_list[1],0],
-    #                      [0, 1*joint_angle_list[2],0],
-    #                      [0, 1*joint_angle_list[3],0],
-    #                      [-1*joint_angle_list[4],0,0]])
-
-    n_joints = len(joint_angle_list)
+    link_rotation = np.multiply(rotational_axes, np.tile(np.array(joint_angle_list).reshape(num_joints, 1), (1, 3)))
 
     Jv = np.empty((3, 0))
     Jw = np.empty((3, 0))
 
-    R_0i = np.eye(3)
+    # R_0i = np.eye(3)
+    T_0n = getWristPose(joint_angle_list, rotational_axes, link_translation).reshape(4,4).T
+    t_0n = T_0n[0:3, 3]
 
-    for i in range(n_joints):
-        if i >= 1:
-            T_0i_flattened = getWristPose(joint_angle_list[0:i], link_translation[0:i], link_rotation[0:i])
-            T_0i = T_0i_flattened.reshape(4, 4).T
-            R_0i = T_0i[0:3, 0:3]
-        
-        Jw = np.append(Jw, np.dot(R_0i, np.array([0, 0, 1]).reshape(3, 1)), axis=1)
+    for i in range(num_joints):
+
+        T_0i_flattened = getWristPose(joint_angle_list[0:i+1], rotational_axes[0:i+1], link_translation[0:i+1])
+        T_0i = T_0i_flattened.reshape(4, 4).T
+
+        R_0i = T_0i[0:3, 0:3]
+        t_0i = T_0i[0:3, 3]
+
+        Jv = np.append(Jv, np.cross(np.dot(R_0i, rotational_axes[i]).reshape(1, 3), (t_0n - t_0i).reshape(1, 3)).reshape(3, 1), axis=1)
+        Jw = np.append(Jw, np.dot(R_0i, rotational_axes[i]).reshape(3, 1), axis=1)
+
     
-    print(Jw)
+    J = np.append(Jv, Jw, axis=0)
+    return J
 
 def main(args):
-    # joint_angles = args.joints
-    joint_angles = [0.727, 1.073, 0.694, -0.244, 1.302]
+    joint_angles = args.joints
     assert len(joint_angles) == 5, "Incorrect number of joints specified."
 
     # TODO("Change this as required")
     link_translation = np.array([[0,      0,      0.072],
-                            [0,      0,      0.04125],
-                            [0.05,   0,      0.2],
-                            [0.2002, 0,      0],
-                            [0.063,  0.0001, 0]])
+                                 [0,      0,      0.04125],
+                                 [0.05,   0,      0.2],
+                                 [0.2002, 0,      0],
+                                 [0.063,  0.0001, 0]])
 
-    link_rotation = np.array([[0, 0, 1*joint_angles[0]],
-                         [0, 1*joint_angles[1],0],
-                         [0, 1*joint_angles[2],0],
-                         [0, 1*joint_angles[3],0],
-                         [-1*joint_angles[4],0,0]])
+    rotational_axes = np.array([[0,  0, 1],
+                                [0,  1, 0],
+                                [0,  1, 0],
+                                [0,  1, 0],
+                                [-1, 0, 0]])
 
-    pose = getWristPose(joint_angles, link_translation, link_rotation)
-    jacobian = getWristJacobian(joint_angles, link_translation, link_rotation)
+    pose = getWristPose(joint_angles, rotational_axes, link_translation)
+    jacobian = getWristJacobian(joint_angles, rotational_axes, link_translation)
 
-    print("Wrist pose: {}".format(np.array_str(np.array(pose), precision=2)))
-    print("Jacobian: {}".format(
-        np.array_str(np.array(jacobian), precision=2)))
+    print("Wrist pose: {}".format(np.array_str(np.array(pose), precision=3)))
+    print("Jacobian: {}".format(np.array_str(np.array(jacobian), precision=3)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(

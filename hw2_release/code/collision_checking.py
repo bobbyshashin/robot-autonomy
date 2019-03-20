fafad055fff93c3fe4cd203ac31e974390a59695
Changsheng Shen <changshs@andrew.cmu.edu>
2019-03-19 20:06:25 -0400

    Fixed collision check

--- hw2_release/code/collision_checking.py
+++ hw2_release/code/collision_checking.py
@@ -13,7 +13,7 @@ import numpy as np
 from numpy import cos,sin
 # import vrep_utils as vu
 
-def constructRotationMatrix(orientation):
+def constructRotationMatrix(orientation, rotation_convention):
     r, p, y = orientation
 
     Rx = np.array([[1.0, 0.0, 0.0],
@@ -27,8 +27,10 @@ def constructRotationMatrix(orientation):
     Rz = np.array([[cos(y), -sin(y), 0.0],
                   [sin(y), cos(y), 0.0],
                   [0.0, 0.0, 1.0]])
-
-    return Rz @ Ry @ Rx
+    if rotation_convention is "ZYX":
+        return Rz @ Ry @ Rx
+    elif rotation_convention is "XYZ": # for vrep
+        return Rx @ Ry @ Rz
 
 def constructVertices(dimension):
     dx, dy, dz = dimension
@@ -43,21 +45,19 @@ def constructVertices(dimension):
 
     dim = np.array([dx * 0.5, dy * 0.5, dz * 0.5]).reshape(3, 1)
 
-    return (vertices_constructor_matrix * np.tile(dim, (1, 8))).transpose()
-
+    return (vertices_constructor_matrix * np.tile(dim, (1, 8)))
 
 class Cuboid:
-    def __init__(self, origin, orientation, dimension):
+    def __init__(self, origin, orientation, dimension, rotation_convention="ZYX"):
 
         self.origin = np.array([origin[0], origin[1], origin[2]])
 
         self.rpy = np.array([orientation[0], orientation[1], orientation[2]])
-        self.R = constructRotationMatrix(orientation)
+        self.R = constructRotationMatrix(orientation, rotation_convention)
 
         self.dimension = np.array([dimension[0], dimension[1], dimension[2]])
-        self.vertices = constructVertices(self.dimension) @ self.R + self.origin # 8x3 matrix, each row is the [x, y, z] for one vertex
 
-
+        self.vertices = (self.R @ constructVertices(self.dimension) + self.origin.reshape(3,1)).transpose() # 8x3 matrix, each row is the [x, y, z] for one vertex
 
 def constructCuboid(origin, orientation, dimension):
 
@@ -73,10 +73,8 @@ def singleAxisCollisionCheck(axis, c1, c2):
     num_vertices = c1.vertices.shape[0]
 
     v1 = c1.vertices
-    # print(v1)
     v2 = c2.vertices
-    # print(v1.shape)
-    # print()
+
     max1 = float('-inf')
     min1 = float('inf')
     max2 = float('-inf')
@@ -85,31 +83,16 @@ def singleAxisCollisionCheck(axis, c1, c2):
     for i in range(num_vertices):
 
         dist1 = v1[i] @ axis
-        # print("v1[i]: ", v1[i])
-        # print("axis: ", axis)
-        # print("dist1: ", dist1)
         dist2 = v2[i] @ axis
-        # print("v2[i]: ", v2[i])
-        # print("axis: ", axis)
-        # print("dist2: ", dist2)
-        if dist1 > max1:
-            max1 = dist1
-        if dist1 < min1:
-            min1 = dist1
-        if dist2 > max2:
-            max2 = dist2
-        if dist2 < min2:
-            min2 = dist2
 
-    # print("max1: ", max1)
-    # print("min1: ", min1)
-    # print("max2: ", max2)
-    # print("min2: ", min2)
+        if dist1 > max1: max1 = dist1
+        if dist1 < min1: min1 = dist1
+        if dist2 > max2: max2 = dist2
+        if dist2 < min2: min2 = dist2
 
     longSpan = max(max1, max2) - min(min1, min2)
     sumSpan = max1 - min1 + max2 - min2
-    # print(longSpan)
-    # print(sumSpan)
+
     return longSpan <= sumSpan
 
 
@@ -139,22 +122,15 @@ def findNormalVectors(c1, c2):
     normals = np.append(normals, c1_axes, axis=1)
     normals = np.append(normals, c2_axes, axis=1)
 
-    # for i in range(15):
-    #     print(normals[:,i])
     return normals # 3xn matrix, each column is a normal vector (either of one surface of a cuboid, or of a pair of edges of two cuboids)
 
 def collisionCheck(c1, c2):
 
     axes_to_check = findNormalVectors(c1, c2)
-    # for row in axes_to_check.T:
-    #     print(row)
 
     collide = True
     for i in range(axes_to_check.shape[1]):
-        # print('i= ', i)
-        # print("Axis: ", axes_to_check[:, i])
         yes = singleAxisCollisionCheck(axes_to_check[:, i], c1, c2)
-        # print(yes)
         collide = collide and yes
     # print(collide)
     return collide
@@ -175,61 +151,15 @@ c6 = Cuboid(origin=[1.8, 0.5, 1.5], orientation=[-0.2, 0.5, 0.0], dimension=[1.0
 c7 = Cuboid(origin=[0.0, -1.2, 0.4], orientation=[0.0, 0.785, 0.785], dimension=[1.0, 1.0, 1.0])
 c8 = Cuboid(origin=[-0.8, 0.0, -0.5], orientation=[0.0, 0.0, 0.2], dimension=[1.0, 0.5, 0.5])
 
-
 # c_test = Cuboid(origin=[-1.5, 1.5, 0.0], orientation=[0.0, 0.0, 0], dimension=[1.0, 2.9, 3.0])
-
-
-# axes = np.eye(3) @ c_test.R
-# print(axes)
-# columns are the x, y, z axes in world frame, after rotation by R
 # collisionCheck(c2, c_test)
 
-# collisionCheck(c_ref, c1)
-# collisionCheck(c_ref, c2)
-# collisionCheck(c_ref, c3)
-# collisionCheck(c_ref, c4)
-# collisionCheck(c_ref, c5)
-# collisionCheck(c_ref, c6)
-# collisionCheck(c_ref, c7)
-# collisionCheck(c_ref, c8)
-
-
-'''
-    debug
-'''
-
-
-# vertices_constructor_matrix = np.array([[ 1,  1,  1],
-#                                         [ 1,  1, -1],
-#                                         [ 1, -1,  1],
-#                                         [-1,  1,  1],
-#                                         [ 1, -1, -1],
-#                                         [-1,  1, -1],
-#                                         [-1, -1,  1],
-#                                         [-1, -1, -1]]).T
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-
-# lines = [(0, 1), (0, 2), (0, 3), (7, 6), (7, 5), (7, 4), (1, 5), (1, 6), (2, 4), (2, 7), (5,7), (4,7)]
-
-# v1 = c_ref.vertices
-# v2 = c6.vertices
-# fig = plt.figure(1)
-# ax = fig.add_subplot(111, projection='3d')
-
-# for line in lines:
-#     coords = np.stack([v1[line[0], :], v1[line[1], :]])
-#     ax.plot(coords[:, 0], coords[:, 1], coords[:, 2], color='r', linewidth=2)
-
-#     coords = np.stack([v1[line[1], :], v1[line[0], :]])
-#     ax.plot(coords[:, 0], coords[:, 1], coords[:, 2], color='r', linewidth=2)
-
-# for line in lines:
-#     coords = np.stack([v2[line[0], :], v2[line[1], :]])
-#     ax.plot(coords[:, 0], coords[:, 1], coords[:, 2], color='b', linewidth=2)
-#     coords = np.stack([v2[line[1], :], v2[line[0], :]])
-#     ax.plot(coords[:, 0], coords[:, 1], coords[:, 2], color='b', linewidth=2)
-
-
-# ax.view_init(azim=-90., elev=90.)
-# plt.show()
+print("======== Collision Checking For 8 Cuboids ========")
+collisionCheck(c_ref, c1)
+collisionCheck(c_ref, c2)
+collisionCheck(c_ref, c3)
+collisionCheck(c_ref, c4)
+collisionCheck(c_ref, c5)
+collisionCheck(c_ref, c6)
+collisionCheck(c_ref, c7)
+collisionCheck(c_ref, c8)
--- hw2_release/code/main.py
+++ hw2_release/code/main.py
@@ -21,140 +21,7 @@ import matplotlib.pyplot as plt
 from PRM import *
 from forward_kinematics import *
 from collision_checking import collisionCheck, Cuboid
-
-###############################################################################
-
-class ArmController:
-
-    def __init__(self):
-        # Fill out this method ##################################
-        # Define any variables you may need here for feedback control
-        # ....
-
-        # For revolute joints
-        self.Kp = 10.0
-        self.Ki = 0.01
-        self.Kd = 0.1
-
-        # For prismatic joints
-        self.Kp_pris = 10.0
-        self.Ki_pris = 0.01
-        self.Kd_pris = 0.1
-
-
-        self.p_term = 0.0
-        self.i_term = 0.0
-        self.d_term = 0.0
-
-        self.i_limit = 1.0
-
-        self.last_timestamp = None
-        self.last_error = None
-        self.first_flag = True
-
-        self.converged_time_interval = 1.5
-        self.converged_time_buffer = 0.0
-        self.time_flag = False
-
-        #########################################################
-        # Do not modify the following variables
-        self.history = {'timestamp': [],
-                        'joint_feedback': [],
-                        'joint_target': [],
-                        'ctrl_commands': []}
-        self._target_joint_positions = None
-
-    def set_target_joint_positions(self, target_joint_positions):
-        assert len(target_joint_positions) == vu.N_ARM_JOINTS, \
-            'Expected target joint positions to be length {}, but it was length {} instead.'.format(len(target_joint_positions), vu.N_ARM_JOINTS)
-        self._target_joint_positions = target_joint_positions
-
-    def calculate_commands_from_feedback(self, timestamp, sensed_joint_positions):
-        assert self._target_joint_positions, \
-            'Expected target joint positions to be set, but it was not.'
-
-        # Fill out this method ##################################
-        # Using the input joint feedback, and the known target joint positions,
-        # calculate the joint commands necessary to drive the system towards
-        # the target joint positions.
-        ctrl_commands = np.zeros(vu.N_ARM_JOINTS)
-        # ...
-
-        error = np.array(self._target_joint_positions) - np.array(sensed_joint_positions)
-        # print("Error: ", error)
-
-        if self.first_flag == True:
-            self.last_timestamp = timestamp
-            self.last_error = error
-            self.first_flag = False
-            return ctrl_commands
-
-        dt = timestamp - self.last_timestamp
-
-        self.p_term = error
-        self.i_term += dt * error
-
-        for i in range(self.i_term.shape[0]):
-            if self.i_term[i] <= -self.i_limit:
-                self.i_term[i] = -self.i_limit
-            elif self.i_term[i] >= self.i_limit:
-                self.i_term[i] = self.i_limit
-
-        self.d_term = (error - self.last_error) / dt
-
-
-        self.last_timestamp = timestamp
-        self.last_error = error
-
-        ctrl_commands[0:-2] = self.Kp * self.p_term[0:-2] + self.Ki * self.i_term[0:-2] + self.Kd * self.d_term[0:-2]
-        # different gains for prismatic joints
-        ctrl_commands[-2:] = self.Kp_pris * self.p_term[-2:] + self.Ki_pris * self.i_term[-2:] + self.Kd_pris * self.d_term[-2:]
-
-        # print("Ctrl commands: ", ctrl_commands)
-        #########################################################
-
-        # Do not modify the following variables
-        # append time history
-        self.history['timestamp'].append(timestamp)
-        self.history['joint_feedback'].append(sensed_joint_positions)
-        self.history['joint_target'].append(self._target_joint_positions)
-        self.history['ctrl_commands'].append(ctrl_commands)
-        return ctrl_commands
-
-    def has_stably_converged_to_target(self, timestamp):
-        # Fill out this method ##################################
-        has_stably_converged_to_target = True
-        num_joints = vu.N_ARM_JOINTS
-        if (len(self._target_joint_positions) != num_joints) or (len(self.last_error) != num_joints):
-            print("Error: dimension inconsistent!")
-
-        for i in range(num_joints):
-            # if fabs(self.last_error[i]) >= fabs(self._target_joint_positions[i] * 0.01): # within 1% range
-            if i < num_joints - 2:
-                if fabs(self.last_error[i]) >= 0.01:
-                    has_stably_converged_to_target = False
-            else:
-                if fabs(self.last_error[i]) >= 0.002:
-                    has_stably_converged_to_target = False
-        # timestamp = vu.get_sim_time_seconds(clientID)
-        if has_stably_converged_to_target == True and self.time_flag == False:
-            self.converged_time_buffer = timestamp
-            self.time_flag = True
-
-        if timestamp - self.converged_time_buffer < self.converged_time_interval:
-            has_stably_converged_to_target = False
-        else:
-            self.time_flag = False
-
-        if has_stably_converged_to_target:
-            self.i_term = 0.0
-
-
-        # ...
-        # if has_stably_converged_to_target == True:
-        #     print("F**k yeah!")
-        #########################################################
-        return has_stably_converged_to_target
+from arm_controller import ArmController
 
 def main(args):
     # Connect to V-REP
@@ -215,7 +82,7 @@ def main(args):
         pos = joint_cuboid_positions[i]
         ori = joint_cuboid_orientations[i]
         dim = joint_cuboid_dimensions[i]
-        c = Cuboid(pos, ori, dim)
+        c = Cuboid(pos, ori, dim, "XYZ")
         # print(c.vertices)
         joint_cuboids.append(c)
 
@@ -225,7 +92,7 @@ def main(args):
         pos = obstacle_cuboid_positions[i]
         ori = obstacle_cuboid_orientations[i]
         dim = obstacle_cuboid_dimensions[i]
-        c = Cuboid(pos, ori, dim)
+        c = Cuboid(pos, ori, dim, "XYZ")
         # print(c.vertices)
         obstacle_cuboids.append(c)
 
